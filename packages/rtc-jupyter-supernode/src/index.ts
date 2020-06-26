898a866d0e3f7e3fcf5621b3565cc43d8a350a57
Saul Shanabrook <s.shanabrook@gmail.com>
2020-06-26 11:32:21 -0400

    Execute in one transaction

--- packages/rtc-jupyter-supernode/src/index.ts
+++ packages/rtc-jupyter-supernode/src/index.ts
@@ -4,6 +4,7 @@ import {
   records,
   updateRecord,
   createRecord,
+  withTransaction,
 } from "rtc-node";
 import { schemas, DisplayType } from "rtc-jupyter";
 import * as jupyter from "rx-jupyter";
@@ -48,46 +49,110 @@ async function main(): Promise<void> {
                   console.log("error", response);
                   return;
                 }
-                // TODO: Size
-                updateRecord(datastore, schemas.contents, content.$id, {
-                  name: response.name,
-                  path: response.path,
-                  type: response.type,
-                  writeable: response.writable,
-                  created: response.created,
-                  last_modified: response.last_modified,
-                  mimetype: response.mimetype,
-                  format: response.format,
-                  fetch: false,
-                });
-                if (response.type === "file") {
-                  updateRecord(
-                    datastore,
-                    schemas.text_content,
-                    content.content,
-                    {
-                      // TODO: remove initial text
-                      // TODO: Or make a new one?
-                      content: {
-                        index: 0,
-                        remove: 0,
-                        text: response.content as string,
-                      },
-                    }
-                  );
-                }
-                if (response.type === "notebook") {
-                  const notebookResponse = response.content as commutable.Notebook;
-                  if (notebookResponse.nbformat == 4) {
-                    const cellIDs = notebookResponse.cells.map(
-                      (cell): string => {
-                        if (
-                          cell.cell_type === "markdown" ||
-                          cell.cell_type === "raw"
-                        ) {
+                withTransaction(datastore, () => {
+                  // TODO: Size
+                  updateRecord(datastore, schemas.contents, content.$id, {
+                    name: response.name,
+                    path: response.path,
+                    type: response.type,
+                    writeable: response.writable,
+                    created: response.created,
+                    last_modified: response.last_modified,
+                    mimetype: response.mimetype,
+                    format: response.format,
+                    fetch: false,
+                  });
+                  if (response.type === "file") {
+                    updateRecord(
+                      datastore,
+                      schemas.text_content,
+                      content.content,
+                      {
+                        // TODO: remove initial text
+                        // TODO: Or make a new one?
+                        content: {
+                          index: 0,
+                          remove: 0,
+                          text: response.content as string,
+                        },
+                      }
+                    );
+                  }
+                  if (response.type === "notebook") {
+                    const notebookResponse = response.content as commutable.Notebook;
+                    if (notebookResponse.nbformat == 4) {
+                      const cellIDs = notebookResponse.cells.map(
+                        (cell): string => {
+                          if (
+                            cell.cell_type === "markdown" ||
+                            cell.cell_type === "raw"
+                          ) {
+                            return createRecord(datastore, schemas.cells, {
+                              metadata: cell.metadata,
+                              type: cell.cell_type,
+                              text: {
+                                index: 0,
+                                remove: 0,
+                                text: joinMultiline(cell.source),
+                              },
+                            });
+                          }
+                          const displays: Array<DisplayType> = [];
+                          let result = null;
+                          let error = null;
+                          for (const output of cell.outputs) {
+                            if (output.output_type === "execute_result") {
+                              // TODO: Can results have display id in transient?
+                              // TODO: Can results have different execution count than input?
+                              result = {
+                                data: output.data as commutable.JSONObject,
+                                metadata: output.metadata,
+                              };
+                            } else if (output.output_type == "display_data") {
+                              displays.push({
+                                type: "data",
+                                data: output.data as commutable.JSONObject,
+                                metadata: output.metadata,
+                                display_id: null,
+                              });
+                            } else if (output.output_type == "error") {
+                              error = {
+                                ename: output.ename,
+                                evalue: output.evalue,
+                                traceback: output.traceback,
+                              };
+                            } else {
+                              displays.push({
+                                type: "stream",
+                                name: output.name,
+                                text: joinMultiline(output.text),
+                              });
+                            }
+                          }
+                          const execution = createRecord(
+                            datastore,
+                            schemas.executions,
+                            {
+                              status:
+                                error === null
+                                  ? {
+                                      status: "ok",
+                                      execution_count: cell.execution_count,
+                                      result,
+                                    }
+                                  : { status: "error", ...error },
+                              displays: {
+                                index: 0,
+                                remove: 0,
+                                values: displays,
+                              },
+                              code: joinMultiline(cell.source),
+                            }
+                          );
                           return createRecord(datastore, schemas.cells, {
                             metadata: cell.metadata,
                             type: cell.cell_type,
+                            execution,
                             text: {
                               index: 0,
                               remove: 0,
@@ -95,88 +160,26 @@ async function main(): Promise<void> {
                             },
                           });
                         }
-                        const displays: Array<DisplayType> = [];
-                        let result = null;
-                        let error = null;
-                        for (const output of cell.outputs) {
-                          if (output.output_type === "execute_result") {
-                            // TODO: Can results have display id in transient?
-                            // TODO: Can results have different execution count than input?
-                            result = {
-                              data: output.data as commutable.JSONObject,
-                              metadata: output.metadata,
-                            };
-                          } else if (output.output_type == "display_data") {
-                            displays.push({
-                              type: "data",
-                              data: output.data as commutable.JSONObject,
-                              metadata: output.metadata,
-                              display_id: null,
-                            });
-                          } else if (output.output_type == "error") {
-                            error = {
-                              ename: output.ename,
-                              evalue: output.evalue,
-                              traceback: output.traceback,
-                            };
-                          } else {
-                            displays.push({
-                              type: "stream",
-                              name: output.name,
-                              text: joinMultiline(output.text),
-                            });
-                          }
-                        }
-                        const execution = createRecord(
-                          datastore,
-                          schemas.executions,
-                          {
-                            status:
-                              error === null
-                                ? {
-                                    status: "ok",
-                                    execution_count: cell.execution_count,
-                                    result,
-                                  }
-                                : { status: "error", ...error },
-                            displays: {
-                              index: 0,
-                              remove: 0,
-                              values: displays,
-                            },
-                            code: joinMultiline(cell.source),
-                          }
-                        );
-                        return createRecord(datastore, schemas.cells, {
-                          metadata: cell.metadata,
-                          type: cell.cell_type,
-                          execution,
-                          text: {
+                      );
+                      // TODO: remove cells  and existing metadata
+                      updateRecord(
+                        datastore,
+                        schemas.notebooks,
+                        content.content,
+                        {
+                          nbformat: notebookResponse.nbformat,
+                          nbformatMinor: notebookResponse.nbformat_minor,
+                          metadata: notebookResponse.metadata,
+                          cells: {
                             index: 0,
                             remove: 0,
-                            text: joinMultiline(cell.source),
+                            values: cellIDs,
                           },
-                        });
-                      }
-                    );
-                    // TODO: remove cells  and existing metadata
-                    updateRecord(
-                      datastore,
-                      schemas.notebooks,
-                      content.content,
-                      {
-                        nbformat: notebookResponse.nbformat,
-                        nbformatMinor: notebookResponse.nbformat_minor,
-                        metadata: notebookResponse.metadata,
-                        cells: {
-                          index: 0,
-                          remove: 0,
-                          values: cellIDs,
-                        },
-                      }
-                    );
+                        }
+                      );
+                    }
                   }
-                }
+                });
               },
             });
           fetchingContent.set(content.$id, subscription);
