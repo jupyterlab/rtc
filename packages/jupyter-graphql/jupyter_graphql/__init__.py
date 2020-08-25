98c2f3f961b1434f477fa94cf189fe4f28393514
Saul Shanabrook <s.shanabrook@gmail.com>
2020-08-25 10:46:54 -0400

    Fix creating schema type

--- packages/jupyter-graphql/jupyter_graphql/__init__.py
+++ packages/jupyter-graphql/jupyter_graphql/__init__.py
@@ -3,7 +3,7 @@ GraphQL server for Jupyter
 """
 
 
-from ariadne import load_schema_from_path, make_executable_schema
+from ariadne import load_schema_from_path, make_executable_schema, QueryType
 from ariadne.asgi import GraphQL
 import importlib.resources
 from starlette.applications import Starlette
@@ -11,7 +11,19 @@ from starlette.applications import Starlette
 __version__ = "0.0.0"
 
 with importlib.resources.path("jupyter_graphql", "schema.graphql") as path:
-    schema = load_schema_from_path(path)
+    schema_str = load_schema_from_path(path)
+
+query = QueryType()
+
+
+@query.field("hello")
+def resolve_hello(_, info):
+    request = info.context["request"]
+    user_agent = request.headers.get("user-agent", "guest")
+    return "Hello, %s!" % user_agent
+
+
+schema = make_executable_schema(schema_str, query)
 
 app = Starlette(debug=True)
 app.mount("/graphql", GraphQL(schema, debug=True))
