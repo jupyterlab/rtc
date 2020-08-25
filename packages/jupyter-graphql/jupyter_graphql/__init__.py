61020d5c4e1e2af0f08fb524dfc253c0e12b1933
Saul Shanabrook <s.shanabrook@gmail.com>
2020-08-25 16:04:30 -0400

    Move over executions schema to graphql

--- packages/jupyter-graphql/jupyter_graphql/__init__.py
+++ packages/jupyter-graphql/jupyter_graphql/__init__.py
@@ -15,12 +15,12 @@ with importlib.resources.path("jupyter_graphql", "schema.graphql") as path:
 
 query = QueryType()
 
-execution_status = UnionType("ExecutionStatus")
+# execution_status = UnionType("ExecutionStatus")
 
 
-@execution_status.type_resolver
-def resolve_execution_status(obj, *_):
-    return "ExecutionStatusOK"
+# @execution_status.type_resolver
+# def resolve_execution_status(obj, *_):
+#     return "ExecutionStatusOK"
 
 
 @query.field("execution")
@@ -28,11 +28,14 @@ def resolve_node(_, info, id):
     return {
         "id": id,
         "code": "some code",
-        "status": {"__typename": "ExecutionStatusOK",},
+        "status": {"__typename": "ExecutionStatusPending",},
+        "displays": [
+            {"__typename": "DisplayData", "data": "JSON!", "metadata": "JSON!"}
+        ],
     }
 
 
-schema = make_executable_schema(schema_str, [query, execution_status])
+schema = make_executable_schema(schema_str, [query])
 
 app = Starlette(debug=True)
 app.mount("/graphql", GraphQL(schema, debug=True))
