d8210f29060bb046fcf29e1b3a613ed3bcfd4174
Saul Shanabrook <s.shanabrook@gmail.com>
2020-08-25 21:15:15 -0400

    Fix sending xsrf cookie

--- packages/jupyter-graphql/jupyter_graphql/jupyter_server_extension.py
+++ packages/jupyter-graphql/jupyter_graphql/jupyter_server_extension.py
@@ -6,10 +6,26 @@ import typing
 from .schema import schema
 
 
+# Pass in other settings to graphql playground
 # https://github.com/prisma-labs/graphql-playground#properties
+# https://github.com/prisma-labs/graphql-playground/issues/1073#issuecomment-526509499
+# To get xsrf cookie
+# https://blog.jupyter.org/security-release-jupyter-notebook-4-3-1-808e1f3bb5e2
 NEW_PLAYGROUND_HTML = ariadne.constants.PLAYGROUND_HTML.replace(
     "// options as 'endpoint' belong here",
-    "settings: {'request.credentials': 'include'}",
+    r"""
+settings: {
+    'request.credentials': 'include',
+    'editor.reuseHeaders': true,
+},
+tabs: [{
+    endpoint: '/graphl/',
+    query: '',
+    headers: {
+        "X-XSRFToken": document.cookie.match("\\b_xsrf=([^;]*)\\b")[1]
+    }
+}]
+""".strip(),
 )
 
 
@@ -23,13 +39,6 @@ class GraphQL(ariadne.wsgi.GraphQL):
         return [NEW_PLAYGROUND_HTML.encode("utf-8")]
 
 
-# https://www.tornadoweb.org/en/stable/web.html#tornado.web.FallbackHandler
-# https://ariadnegraphql.org/docs/wsgi
-wsgi_app = tornado.wsgi.WSGIContainer(GraphQL(schema))
-handlers = [
-    (r".*", tornado.web.FallbackHandler, {"fallback": wsgi_app}),
-]
-
 # https://jupyter-server.readthedocs.io/en/latest/developers/extensions.html#distributing-a-server-extension
 def _load_jupyter_server_extension(serverapp):
     """
@@ -37,5 +46,16 @@ def _load_jupyter_server_extension(serverapp):
     """
     # https://github.com/bdarnell/django-tornado-demo/blob/master/testsite/tornado_main.py
 
-    serverapp.web_app.add_handlers(".*$", handlers)
+    serverapp.web_app.add_handlers(
+        r".*$",
+        [
+            (
+                r".*",
+                tornado.web.FallbackHandler,
+                # https://www.tornadoweb.org/en/stable/web.html#tornado.web.FallbackHandler
+                # https://ariadnegraphql.org/docs/wsgi
+                {"fallback": tornado.wsgi.WSGIContainer(GraphQL(schema))},
+            ),
+        ],
+    )
 
