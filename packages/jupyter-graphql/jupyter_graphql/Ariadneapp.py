327011a8044b58c81fb352ef6efb4377f66fbc83
Saul Shanabrook <s.shanabrook@gmail.com>
2020-09-24 12:14:07 -0400

    Add support for websockets/subscripts to tornado

--- packages/jupyter-graphql/jupyter_graphql/Ariadneapp.py
+++ packages/jupyter-graphql/jupyter_graphql/Ariadneapp.py
@@ -8,27 +8,69 @@ Changes:
 3. Changed to use ariadne.graphql over graphql.graphql function to mirror ariedne.asgi code
 """
 
+import dataclasses
+import json
+import typing
 
 import ariadne
 import ariadne.asgi
 import ariadne.constants
 import ariadne.exceptions
 import jupyter_server.base.handlers
+import starlette.websockets
 import tornado.escape
 import tornado.httputil
 import tornado.web
+import tornado.websocket
 
 
-class GraphQLHandler(jupyter_server.base.handlers.JupyterHandler):
+@dataclasses.dataclass
+class MockStarletteWebsocket:
+    """
+    Mock a starlette websocket so we can pass it to the ariadne asgi handler for websockets
+    """
+
+    handler: tornado.websocket.WebSocketHandler
+
+    async def send_json(self, json: dict) -> None:
+        self.handler.write_message(json)
+
+    async def close(self) -> None:
+        self.handler.close()
+
+    @property
+    def application_state(self) -> starlette.websockets.WebSocketState:
+        if (
+            self.handler.ws_connection is None
+            or self.handler.ws_connection.is_closing()
+        ):
+            return starlette.websockets.WebSocketState.DISCONNECTED
+        return starlette.websockets.WebSocketState.CONNECTED
+
+    @property
+    def client_state(self) -> starlette.websockets.WebSocketState:
+        """
+        Not sure if this is right but :shrug: not sure how to get this from tornado
+        """
+        return self.application_state
+
+
+class GraphQLHandler(
+    jupyter_server.base.handlers.JupyterHandler, tornado.websocket.WebSocketHandler
+):
     def initialize(
         self, graphql_app: ariadne.asgi.GraphQL, playground_html: str, *args, **kwargs
     ):
         super().initialize(*args, **kwargs)
         self.graphql_app = graphql_app
         self.playground_html = playground_html
+        self.subscriptions: typing.Dict[str, typing.AsyncGenerator] = {}
 
     @tornado.web.authenticated
     async def get(self):
+        # if upgrading use websockethandler behavior
+        if self.request.headers.get("Upgrade", "").lower() == "websocket":
+            return await super().get()
         self.finish(self.playground_html)
 
     @tornado.web.authenticated
@@ -69,3 +111,16 @@ class GraphQLHandler(jupyter_server.base.handlers.JupyterHandler):
                 "Request body is not a valid JSON"
             )
         return data
+
+    def open(self):
+        pass
+
+    async def on_message(self, message):
+        await self.graphql_app.handle_websocket_message(
+            json.loads(message),
+            typing.cast(starlette.websockets.WebSocket, MockStarletteWebsocket(self)),
+            self.subscriptions,
+        )
+
+    def on_close(self):
+        pass
